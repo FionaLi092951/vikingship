8308e4b0f320a81052745227a8e6855b5dac85f5
Fiona123 <1214962388@qq.com>
2020-09-19 15:57:38 +0800

    add component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/Button';
+import './styles/index.scss';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>不同的ButtonType</h3>
+        <Button btnType={ButtonType.Primary}>Primary</Button>
+        <Button btnType={ButtonType.Default} className="custom">Default</Button>
+        <Button btnType={ButtonType.Danger} onClick={(e) => {e.preventDefault(); alert('123'); }}>Danger</Button>
+        <Button btnType={ButtonType.Link} target="_blank" href="http://www.baidu.com">Link</Button>
+        <h3>不同的ButtonSize</h3>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Small</Button>
+        <Button btnType={ButtonType.Primary}>Normal</Button>
+        <h3>Disable状态</h3>
+        <Button btnType={ButtonType.Primary} disabled={true}>Disable</Button>
+        <Button btnType={ButtonType.Link} disabled={true}>Disable</Button>
       </header>
     </div>
   );
