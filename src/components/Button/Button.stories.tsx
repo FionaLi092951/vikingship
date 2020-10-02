31fc545774bcefa826c4819762d877428a396f46
fangfangli <fangfangli@emotibot.com>
2020-10-02 17:14:45 +0800

    add different stories of Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions';
 
-import Button, { ButtonProps, ButtonType } from './Button';
+import Button, { ButtonProps, ButtonSize, ButtonType } from './Button';
 
 export default {
   title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
+    // backgroundColor: { control: 'color' }
   },
 } as Meta;
 
@@ -23,6 +23,46 @@ export default {
 
 const defaultButton: Story<ButtonProps>  = () => (
   <Button onClick={action('button clicked')}>default button</Button>
-)
+);
 
 export const Default = defaultButton.bind({});
+
+
+const buttonSize: Story<ButtonProps>  = () => (
+  <>
+    <Button size={ButtonSize.Large} onClick={action('button clicked')}>Large</Button>
+    <Button size={ButtonSize.Small} onClick={action('button clicked')}>Small</Button>
+  </>
+);
+
+export const DifferentButtonSize = buttonSize.bind({});
+
+const buttonType: Story<ButtonProps>  = () => (
+  <>
+    <Button btnType={ButtonType.Primary} onClick={action('button clicked')}>Primary</Button>
+    <Button btnType={ButtonType.Danger} onClick={action('button clicked')}>Danger</Button>
+    <Button btnType={ButtonType.Link} onClick={action('button clicked')}>Link</Button>
+    <Button btnType={ButtonType.Default} onClick={action('button clicked')}>Default</Button>
+  </>
+);
+
+export const DifferentButtonType = buttonType.bind({});
+
+const disabledBtn: Story<ButtonProps>  = () => (
+  <>
+    <Button btnType={ButtonType.Primary} disabled onClick={action('button clicked')}>Primary</Button>
+    <Button btnType={ButtonType.Danger} disabled onClick={action('button clicked')}>Danger</Button>
+    <Button btnType={ButtonType.Link} disabled onClick={action('button clicked')}>Link</Button>
+  </>
+);
+
+export const DisabledButton = disabledBtn.bind({});
+
+const roundCornerBtn: Story<ButtonProps>  = () => (
+  <>
+    <Button btnType={ButtonType.Primary} roundCorner onClick={action('button clicked')}>Primary</Button>
+    <Button btnType={ButtonType.Danger} roundCorner onClick={action('button clicked')}>Danger</Button>
+  </>
+);
+
+export const RoundCornerButton = roundCornerBtn.bind({});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,12 +14,31 @@ export enum ButtonType  {
 }
 
 interface IBaseButtonProps {
-	className?: string;
-	disabled?: boolean;
-	size?: string;
+  /**
+   * Add user define class to button
+   */
+  className?: string;
+  /**
+   * button cannot be clicked if it's disabled
+   */
+  disabled?: boolean;
+  /**
+   * size of button: lg sm, default is normal size
+   */
+  size?: string;
+  /**
+   * button type: primary, default, danger, link
+   */
 	btnType?: ButtonType;
-	children?: React.ReactNode;
-	href?: string;
+  /**
+   * href value of button with link type
+   */
+  href?: string;
+  /**
+   * set button as round corner or not
+   */
+  roundCorner?: boolean;
+  children?: React.ReactNode;
 }
 
 type NativeButtonProps = IBaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
@@ -32,7 +51,8 @@ const Button: React.FC<ButtonProps> = (props) => {
 		btnType,
 		disabled,
 		size,
-		children,
+    children,
+    roundCorner,
 		href,
 		className,
 		...restProps
@@ -40,7 +60,8 @@ const Button: React.FC<ButtonProps> = (props) => {
 	// btn, btn-big, btn-primary
 	const classes = classNames('btn', className, {
 		[`btn-${btnType}`]: btnType,
-		[`btn-${size}`]: size,
+    [`btn-${size}`]: size,
+    'round-corner': roundCorner,
 		'disabled': (btnType === ButtonType.Link) && disabled
 	});
 	if (btnType === ButtonType.Link && href) {
