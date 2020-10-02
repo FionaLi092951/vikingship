3a5c60840b6778515505f7ba6f908fb742133c63
fangfangli <fangfangli@emotibot.com>
2020-10-02 15:20:42 +0800

    add test case for Menu component

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
+import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react';
 import Menu, { MenuProps } from './Menu';
 import MenuItem from './MenuItem';
+import SubMenu from './SubMenu';
 
 const testProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   onSelect: jest.fn(),
   className: 'test',
 };
 
 const testVerProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   mode: 'vertical',
 };
 
@@ -20,16 +21,34 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>other</MenuItem>
-      <li>hello</li>
+      <SubMenu title="SubMenu">
+        <MenuItem>SubMenu1</MenuItem>
+        <MenuItem disabled>SubMenu2</MenuItem>
+        <MenuItem>SubMenu3</MenuItem>
+      </SubMenu>
     </Menu>
   )
 }
 
+const createStyleFile = () => {
+  const cssFile: string = `
+  .viking-submenu {
+    display: none;
+  }
+  .viking-submenu.menu-opened{
+    display: block;
+  }`;
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = cssFile;
+  return style;
+}
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
 
 describe('test Menu and MenuItem components', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuElement = wrapper.getByTestId('test-menu');
     activeElement = wrapper.getByText('active');
     disabledElement = wrapper.getByText('disabled');
@@ -37,24 +56,41 @@ describe('test Menu and MenuItem components', () => {
   it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('viking-menu test');
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
-    expect(activeElement).toHaveClass('viking-menu-item is-active');
-    expect(disabledElement).toHaveClass('viking-menu-item is-disabled');
+    // expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
+    expect(activeElement).toHaveClass('menu-item is-active');
+    expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
   it('click items should change active and call the right callback', () => {
     const thirdItem = wrapper.getByText('other');
     fireEvent.click(thirdItem);
     expect(thirdItem).toHaveClass('is-active');
     expect(activeElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    expect(testProps.onSelect).toHaveBeenCalledWith('2');
     fireEvent.click(disabledElement);
     expect(disabledElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
   });
   it('should render vertical mode when is set to vertical', () => {
     cleanup();
     const wrapper = render(generateMenu(testVerProps));
     menuElement = wrapper.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
+  });
+  it('should show dropdown item when hover on subMenu', async () => {
+    const dropdownItem1 = wrapper.queryByText('SubMenu1');
+    expect(dropdownItem1).not.toBeVisible();
+    const dropdownElement = wrapper.queryByText('SubMenu');
+    fireEvent.mouseEnter(dropdownElement);
+    // 等待timer执行结束 或 timer超时等
+    await wait(() => {
+      expect(dropdownItem1).toBeVisible();
+    });
+    fireEvent.click(dropdownItem1);
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+    fireEvent.mouseLeave(dropdownElement);
+    await wait(() => {
+      expect(dropdownItem1).not.toBeVisible();
+    });
   });
 });
