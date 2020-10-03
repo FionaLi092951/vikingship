ab6b68298d9392057df031302a46eb697c23ebb9
fangfangli <fangfangli@emotibot.com>
2020-10-03 14:58:36 +0800

    update Button.stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 
 import Button, { ButtonProps, ButtonSize, ButtonType } from './Button';
 
